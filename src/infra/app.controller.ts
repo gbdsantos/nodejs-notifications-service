7782a4409a59054ed37fdf41bce06421ec73e1a7
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-12-14 19:07:53 -0300

    🚚 update(backend): change folders structures

--- src/infra/app.controller.ts
+++ src/infra/app.controller.ts
@@ -6,7 +6,7 @@ import { CreateNotificationBody } from './create-notification-body';
 
 @Controller('notifications')
 export class AppController {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) { }
 
   @Get()
   list() {
